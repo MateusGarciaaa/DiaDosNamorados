f63f317206d7669b1a262cf76aaf5ac8eff55cd7
MateusGarciaaa <mateusgarcia3081@gmail.com>
2025-06-12 11:06:00 -0300

    me ajuda

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ document.getElementById("btn-reveal").addEventListener("click", () => {
 });
 
 function startLiveCounter() {
-  const startDate = new Date("2023-12-31T00:00:01");
+  const startDate = new Date("2023-12-31T00:00:00");
 
   function getTimeDifference(startDate, now) {
     let start = new Date(startDate);
