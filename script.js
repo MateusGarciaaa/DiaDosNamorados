f18e8b4d92eb889f97c43d5cdb14dabbde6340e8
MateusGarciaaa <mateusgarcia3081@gmail.com>
2025-06-12 01:01:05 -0300

    javascript atualizado

--- script.js
+++ script.js
@@ -47,7 +47,7 @@ const prevBtn = document.querySelector('.carousel-btn.prev');
 const nextBtn = document.querySelector('.carousel-btn.next');
 
 let index = 0;
-const totalSlides = 10;
+const totalSlides = 11;
 
 function updateCarousel() {
   const slideWidth = track.clientWidth / totalSlides;
